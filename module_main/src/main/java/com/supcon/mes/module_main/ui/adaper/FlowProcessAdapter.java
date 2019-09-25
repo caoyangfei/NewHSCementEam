1c89dfaae0e39470aa1b69d84b6089a1ab58dbf6
caoyangfei <cao18317713272@163.com>
2019-09-25 09:19:30 +0800

    张文帅
    流程时间显示 无年

--- module_main/src/main/java/com/supcon/mes/module_main/ui/adaper/FlowProcessAdapter.java
+++ module_main/src/main/java/com/supcon/mes/module_main/ui/adaper/FlowProcessAdapter.java
@@ -3,6 +3,7 @@ package com.supcon.mes.module_main.ui.adaper;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -107,7 +108,11 @@ public class FlowProcessAdapter extends BaseListDataRecyclerViewAdapter<FlowProc
 
             }
             itemFlowName.setText(data.flowProcess);
-            itemFlowTime.setText(data.time);
+            if (!TextUtils.isEmpty(data.time)){
+                itemFlowTime.setText(data.time.substring(5));
+            }else {
+                itemFlowTime.setText("--");
+            }
         }
     }
 }
